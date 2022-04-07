5a22303439d0a21983ec4e235053e8a65df9be4a
xiaowang <1577142422@qq.com>
2022-04-07 10:57:42 +0800

    fix: (markdown):修改demo内容

--- src/components/markdown/demo/basic.jsx
+++ src/components/markdown/demo/basic.jsx
@@ -1,4 +1,9 @@
 import React from 'react';
 import { Markdown } from 'wly-ui';
 
-export default () => <Markdown># Headering</Markdown>;
+export default () => (
+  <Markdown>{`# Scrollbar\r
+## Custom scrollbar\r
+---\r
+Custom scrollbars are getting more popular nowadays, and I’m very keen to dig into them. There are different reasons why to customize a scrollbar. `}</Markdown>
+);
