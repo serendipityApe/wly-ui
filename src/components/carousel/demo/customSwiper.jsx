868810dfecb3d72ea869839981de3d3aaa75a844
xiaowang <1577142422@qq.com>
2022-05-27 10:44:05 +0800

    fix(carousel): 修复错误上传的文件

--- src/components/carousel/demo/customSwiper.jsx
+++ src/components/carousel/demo/customSwiper.jsx
@@ -9,29 +9,29 @@ const imgList = [
 export default () => (
   <Carousel
     grallery={
-      // <Grallery
-      //   showQuantity={3}
-      //   gap={10}
-      //   itemWidth={45}
-      //   itemClick={(k,swiperTo) => {
-      //     // setActiveKey(k);
-      //     swiperTo(k);
-      //     console.log(k);
-      //   }}
-      //   active="swiper-active"
-      // >
-      //   {imgList.map(item => (
-      //     <img style={{ borderRadius: '0.75rem' }} key={item + 'dots'} src={item} alt="" />
-      //   ))}
-      // </Grallery>
-      <div>
-        <div className="1" onClick={e => console.log(e._swiperTo)}>
-          1
-        </div>
-        <div className="2">
-          2
-        </div>
-      </div>
+      <Grallery
+        showQuantity={3}
+        gap={10}
+        itemWidth={45}
+        itemClick={(k,swiperTo) => {
+          // setActiveKey(k);
+          swiperTo(k);
+          console.log(k);
+        }}
+        active="swiper-active"
+      >
+        {imgList.map(item => (
+          <img style={{ borderRadius: '0.75rem' }} key={item + 'dots'} src={item} alt="" />
+        ))}
+      </Grallery>
+      // <div>
+      //   <div className="1" onClick={e => console.log(e._swiperTo)}>
+      //     1
+      //   </div>
+      //   <div className="2">
+      //     2
+      //   </div>
+      // </div>
     }
   >
     {imgList.map(item => (
